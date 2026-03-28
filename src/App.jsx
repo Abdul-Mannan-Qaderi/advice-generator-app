5ed56ca8724c5b016a618dfe2a35740ef895475b
Abdul Mannan <fahadfateh1000@gmail.com>
2026-03-28 15:06:00 +0430

    some refinement

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ export default function App() {
 	const [advice, setAdvice] = useState(null);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(null);
+	let ad = advice;
+	let i = advice?.slip?.id;
 
 	function handleWidth(e) {
 		console.log(e);
@@ -29,14 +31,6 @@ export default function App() {
 	}, []);
 	console.log(advice);
 
-	if (loading) {
-		return (
-			<p className="text-center mt-20 text-4xl text-tomato/50">Loading ...</p>
-		);
-	}
-	if (error) {
-	}
-
 	return (
 		<main
 			className="bg-Blue-950 min-h-dvh text-Blue-200 flex justify-center items-center p-5 min-w-[375px]"
@@ -45,17 +39,17 @@ export default function App() {
 			{error ? (
 				<p className="text-center mt-20 text-4xl text-tomato">{error}</p>
 			) : loading ? (
-				<p className="text-center mt-20 text-4xl text-tomato/50">Loading ...</p>
+				<p className="text-center mt-20 text-4xl text-Green-300/50">Loading ...</p>
 			) : (
 				<div className="bg-Blue-900 p-7 sm:pb-10 sm:w-lg pb-12 rounded-2xl text-center relative w-sm sm:px-10">
 					{/* Advice #<!-- Advice ID goes here --> */}
 					<p className="text-Green-300 tracking-[4px] font-semibold text-sm mb-7 mt-5">
-						ADVICE #{advice?.slip?.id}
+						ADVICE #{!loading ? advice?.slip?.id : i}
 					</p>
 					{/* "<!-- Advice text goes here -->" */}
 					<p className="text-[28px] font-extrabold">
 						<span className="">&#8220;</span>
-						{advice?.slip?.advice}&#8221;
+						{!loading ? advice?.slip?.advice : ad}&#8221;
 					</p>
 					<div>
 						<picture>
